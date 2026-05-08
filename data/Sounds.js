2fee690c53e2c71ebf6261c22f94491cbd0d95a1
Bill Fraser <bfraser@alumni.ucsd.edu>
2026-05-08 13:51:00 -0700

    Switching up the sounds, to see what happens.  Debugging whistle trouble.

--- data/Sounds.js
+++ data/Sounds.js
@@ -10,8 +10,8 @@ Sounds = {
   pass : loadSound('Pass.mp3'),
   play : loadSound('Play.mp3'),
   retire : loadSound('Retire.mp3'),
-  shuffle : loadSound('Shuffle.mp3'),
+  shuffle : loadSound('Whistle.mp3'),
   timerPaused : loadSound('TimerPaused.mp3'),
-  whistle : loadSound('Whistle.mp3'),
+  whistle : loadSound('Flute.mp3'),
   yourCard : loadSound('YourCard.mp3'),
 }
